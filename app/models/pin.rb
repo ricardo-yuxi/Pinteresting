e72622b01481b8504c19e972acdfd91495639a69
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2016-01-18 11:55:27 -0500

    change credential ^ configuration pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,12 @@ class Pin < ActiveRecord::Base
   has_attached_file :image,
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                    :dropbox_credentials => { app_key: ENV['APP_KEY'],
+                              app_secret: ENV['APP_SECRET'],
+                              access_token: ENV['ACCESS_TOKEN'],
+                              access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+                              user_id: ENV['USER_ID'],
+                              access_type: 'app_folder'}
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
